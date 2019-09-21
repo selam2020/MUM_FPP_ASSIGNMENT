25dd8a27e900658e85ec3302b10c9244bc5d7855
selam2020 <55061854+selam2020@users.noreply.github.com>
2019-09-21 03:18:40 -0500

    probg9prob1

---
+++ prog9prob1/QueueException.java
@@ -0,0 +1,19 @@
+package prog9prob1;
+
+
+	
+	@SuppressWarnings("serial")
+	public class QueueException extends Exception {
+		public QueueException() {
+			super();
+		}
+		public QueueException(String msg) {
+			super(msg);
+		}
+		public QueueException(Throwable t) { 
+			super(t);
+		}
+	}
+
+
+
